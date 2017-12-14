35b8fcff0bcdbd95f446e8e73fd1aa2342bb7b29
Robbin <robbinrijkee@gmail.com>
2017-12-14 10:28:42 +0100

    Opdracht 2.1

---
+++ JavaScript/JavascriptDOM/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("subkop"). innerText = "Dit is de subkop";
+document.getElementById("aap1").src = "img/pug-801826_1920.jpg";
+document.getElementById("aap2").src = "img/wild_hair.jpg";
