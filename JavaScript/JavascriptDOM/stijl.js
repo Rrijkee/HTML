9939edc654578e37abc9958d5483027836c7f951
Robbin <robbinrijkee@gmail.com>
2017-12-14 10:52:56 +0100

    Opdracht 2.2

---
+++ JavaScript/JavascriptDOM/stijl.js
@@ -0,0 +1,12 @@
+document.getElementById("bl1").style.backgroundColor = "aqua";
+
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.marginTop = "20px";
+
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.marginTop = "40px";
+
+
+document.getElementById("bl4").style.backgroundColor = "red";
+
+
